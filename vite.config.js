5bd98cee4e26430a45690942b55360c2c1e43b2a
Pele <102541131+PeleBahalag@users.noreply.github.com>
2025-08-18 13:28:00 +0300

    added 404 workaround

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   base :'/TrainingPacket/',
-  plugins: [vue()],
+  plugins: [vue(),
+    {
+      name: 'spa-fallback',
+      closeBundle() {
+        const distDir = resolve(__dirname, 'dist')
+        fs.copyFileSync(
+          resolve(distDir, 'index.html'),
+          resolve(distDir, '404.html')
+        )
+      }
+    }
+  ],
   
 })
